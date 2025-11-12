44a14844f8f05228e5830df24df756f9a494d57e
aniketlimkar26 <aniketlimkar9068@gmail.com>
2025-11-12 20:02:14 +0530

    Sum of Numbers

---
+++ scratch.py
@@ -0,0 +1,13 @@
+#find the sum of all elements in a list.
+numbers= [2, 4, 6, 8, 10]
+total= 0
+for num in numbers:
+    total+=num
+print("Sum of all elements:", total)
+
+
+#another way
+numbers = [2, 4, 6, 8, 10]
+total = sum(numbers)
+print("Sum of all elements:", total)
+
