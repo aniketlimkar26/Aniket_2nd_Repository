04fcbde51693ff46674d0c3689000d96eed4fc26
aniketlimkar26 <aniketlimkar9068@gmail.com>
2025-11-12 19:45:47 +0530

    this is my second commit

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-a=2
-b=3
-c=a+b
-print(c)
+def program1():
+    a=2
+    b=3
+    c=a+b
+    print(c)
+
+
+def program2():
+    # Write a Python program to check if a number is even or odd.
+    num= int(input("Enter a number: "))
+    if num%2==0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+program2()
